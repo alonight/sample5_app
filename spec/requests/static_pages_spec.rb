ba7ceeacbcb8a48cd2d853889822f8ced938cada
alonight <alonight@live.cn>
2014-05-07 20:19:36 +0800

    5.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
   end
 
