9dc84c782faad8a7eb2a30a02fada7dec07e845f
alonight <alonight@live.cn>
2014-05-14 20:36:23 +0800

    5.5-3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,7 @@ describe "Static pages" do
 
     before { visit root_path }
     let(:heading) { "Sample App" }
-    let(:page_title) { ' ' }
+    let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
@@ -23,16 +23,40 @@ describe "Static pages" do
   describe "Help page" do
 
     before { visit help_path }
-    let(:heading)
+    let(:heading) { "Help" }
+    let(:page_title) { "Help" }
+
+    it_should_behave_like "all static pages"
+
   end
+
   describe "About page" do
     before { visit about_path }
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading) { "About" }
+    let(:page_title) { "About Us" }
+
+    it_should_behave_like "all static pages"
+
   end
+
   describe "Contact page" do
     before { visit contact_path }
+
     it { should have_selector('h1',text:'Contact') }
     it { should have_title(full_title('Contact')) }
+
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App")
   end
 end
