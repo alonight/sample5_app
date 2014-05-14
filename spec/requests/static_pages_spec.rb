339792489d15e6edfde5efa99f2e4ea5cef511f2
alonight <alonight@live.cn>
2014-05-14 19:04:19 +0800

    5.6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   subject { page }
-  describe "Home page" do
-    before { visit root_path }
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
   end
+
+  describe "Home page" do
+
+    before { visit root_path }
+    let(:heading) { "Sample App" }
+    let(:page_title) { ' ' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+
+  end
+
   describe "Help page" do
+
     before { visit help_path }
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading)
   end
   describe "About page" do
     before { visit about_path }
@@ -20,7 +32,7 @@ describe "Static pages" do
   end
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact') }
+    it { should have_selector('h1',text:'Contact') }
     it { should have_title(full_title('Contact')) }
   end
 end
