b592634b6f73986908e438928ed8a05afae9d8fd
alonight <alonight@live.cn>
2014-10-29 22:25:21 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Sample5App::Application.routes.draw do
 
-  get "users/new"
+  resources :users
   root to: 'static_pages#home'
   match '/help',  to: 'static_pages#help',  via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
