bed0ce939967b2be5da3c5c3ee9fef02c2822943
alonight <alonight@live.cn>
2014-11-12 23:03:50 +0800

    10.3.2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Sample5App::Application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
-  #resources :microposts, only: [:create, destroy]
+  resources :microposts, only: [:create, :destroy]
   root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
